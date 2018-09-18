8d339fd3c8bb0306d7e6efd9dd1861bcae051d2f
frank <frankwco@gmail.com>
2018-09-18 12:52:00 -0300

    ajustes

--- src/main/java/base/modelo/Indicador.java
+++ src/main/java/base/modelo/Indicador.java
@@ -55,6 +55,115 @@ public class Indicador implements Serializable {
 	private String formulaGrupoLancamento = "";
 
 	private String formulaIndicador = "";
+	
+	private Double meta01;
+	private Double meta02;
+	private Double meta03;
+	private Double meta04;
+	private Double meta05;
+	private Double meta06;
+	private Double meta07;
+	private Double meta08;
+	private Double meta09;
+	private Double meta10;
+	private Double meta11;
+	private Double meta12;
+	
+	public Double getMeta01() {
+		return meta01;
+	}
+
+	public void setMeta01(Double meta01) {
+		this.meta01 = meta01;
+	}
+
+	public Double getMeta02() {
+		return meta02;
+	}
+
+	public void setMeta02(Double meta02) {
+		this.meta02 = meta02;
+	}
+
+	public Double getMeta03() {
+		return meta03;
+	}
+
+	public void setMeta03(Double meta03) {
+		this.meta03 = meta03;
+	}
+
+	public Double getMeta04() {
+		return meta04;
+	}
+
+	public void setMeta04(Double meta04) {
+		this.meta04 = meta04;
+	}
+
+	public Double getMeta05() {
+		return meta05;
+	}
+
+	public void setMeta05(Double meta05) {
+		this.meta05 = meta05;
+	}
+
+	public Double getMeta06() {
+		return meta06;
+	}
+
+	public void setMeta06(Double meta06) {
+		this.meta06 = meta06;
+	}
+
+	public Double getMeta07() {
+		return meta07;
+	}
+
+	public void setMeta07(Double meta07) {
+		this.meta07 = meta07;
+	}
+
+	public Double getMeta08() {
+		return meta08;
+	}
+
+	public void setMeta08(Double meta08) {
+		this.meta08 = meta08;
+	}
+
+	public Double getMeta09() {
+		return meta09;
+	}
+
+	public void setMeta09(Double meta09) {
+		this.meta09 = meta09;
+	}
+
+	public Double getMeta10() {
+		return meta10;
+	}
+
+	public void setMeta10(Double meta10) {
+		this.meta10 = meta10;
+	}
+
+	public Double getMeta11() {
+		return meta11;
+	}
+
+	public void setMeta11(Double meta11) {
+		this.meta11 = meta11;
+	}
+
+	public Double getMeta12() {
+		return meta12;
+	}
+
+	public void setMeta12(Double meta12) {
+		this.meta12 = meta12;
+	}
 
 	public String getAbreviacao() {
 		//System.out.println("Abre: "+abreviacao);
