79f9b05744f0398455ab9816232cde816e5bf0db
frank <frankwco@gmail.com>
2018-09-28 12:50:15 -0300

    ajustes

--- src/main/java/base/controle/TesteMB.java
+++ src/main/java/base/controle/TesteMB.java
@@ -10,6 +10,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.primefaces.event.ItemSelectEvent;
+import org.primefaces.model.chart.MeterGaugeChartModel;
 
 import base.modelo.Pessoa;
 import base.modelo.Atividade;
@@ -24,11 +25,15 @@ import dao.GenericDAO;
 public class TesteMB implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private String porcentagem = "20";
 
-	
-	
+	private String statusVermelho = "<i class=\"fa fa-circle\" style=\"font-size:36px;color:red\"></i>";
+	private String statusVerde = "<i class=\"fa fa-circle\" style=\"font-size:36px;color:green\"></i>";
+	private String statusLaranja = "<i class=\"fa fa-circle\" style=\"font-size:36px;color:orange\"></i>";
+
+	private MeterGaugeChartModel meterGaugeModel2;
+
 	@Inject
 	private GenericDAO<Atividade> daoProcesso; // faz as buscas
 
@@ -37,17 +42,45 @@ public class TesteMB implements Serializable {
 
 	@PostConstruct
 	public void inicializar() {
+		createMeterGaugeModels();
+	}
 
+	public MeterGaugeChartModel getMeterGaugeModel2() {
+		return meterGaugeModel2;
 	}
-	
-	public void chamada(){
-		System.out.println("Teste de chamada de método");
+
+	private MeterGaugeChartModel initMeterGaugeModel() {
+		List<Number> intervals = new ArrayList<Number>() {
+			{
+				add(20);
+				add(50);
+				add(120);
+				add(220);
+			}
+		};
+
+		return new MeterGaugeChartModel(140, intervals);
 	}
-	
-	public void itemSelect(ItemSelectEvent event) 
-	{  
+
+	private void createMeterGaugeModels() {
+
+		meterGaugeModel2 = initMeterGaugeModel();
+		meterGaugeModel2.setTitle("Custom Options");
+		meterGaugeModel2.setSeriesColors("66cc66,93b75f,E7E658,cc6666");
+		meterGaugeModel2.setGaugeLabel("km/h");
+		meterGaugeModel2.setGaugeLabelPosition("bottom");
+		meterGaugeModel2.setShowTickLabels(false);
+		meterGaugeModel2.setLabelHeightAdjust(110);
+		meterGaugeModel2.setIntervalOuterRadius(100);
+	}
+
+	public void chamada() {
+		System.out.println("Teste de chamada de mï¿½todo");
+	}
+
+	public void itemSelect(ItemSelectEvent event) {
 		System.out.println("Item Index: " + event.getItemIndex() + ", Series Index:" + event.getSeriesIndex());
-		
+
 	}
 
 	public String getPorcentagem() {
@@ -57,8 +90,29 @@ public class TesteMB implements Serializable {
 	public void setPorcentagem(String porcentagem) {
 		this.porcentagem = porcentagem;
 	}
-	
-	
 
-	
+	public String getStatusVermelho() {
+		return statusVermelho;
+	}
+
+	public void setStatusVermelho(String statusVermelho) {
+		this.statusVermelho = statusVermelho;
+	}
+
+	public String getStatusVerde() {
+		return statusVerde;
+	}
+
+	public void setStatusVerde(String statusVerde) {
+		this.statusVerde = statusVerde;
+	}
+
+	public String getStatusLaranja() {
+		return statusLaranja;
+	}
+
+	public void setStatusLaranja(String statusLaranja) {
+		this.statusLaranja = statusLaranja;
+	}
+
 }
